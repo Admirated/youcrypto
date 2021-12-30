59b641cce0a3a4dc718421c4deeeb1e94b496e68
Vladimir <vovanebozhinsky@gmail.com>
2021-12-30 20:14:43 +0200

    copy

---
+++ token.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', () => {
+   const copyBtn = document.querySelector('.copy__btn');
+
+   copyBtn.addEventListener('click', () => {
+      const address = document.querySelector('.token__info-contract-wrapper p').textContent;
+      setClipboard(address);
+   });
+
+   function setClipboard(text) {
+      var type = "text/plain";
+      var blob = new Blob([text], {
+         type
+      });
+      var data = [new ClipboardItem({
+         [type]: blob
+      })];
+
+      navigator.clipboard.write(data).then(
+         function () {
+            copyBtn.querySelector('svg path').style.fill = "#00e23f";
+         },
+         function () {
+            copyBtn.querySelector('svg path').style.fill = "#e61b1b";
+
+         }
+      );
+   }
+});
