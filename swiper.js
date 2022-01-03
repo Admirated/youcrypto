e20cf5b2219ff6fc52ede73e2aeb647241fb03e5
Vladimir <vovanebozhinsky@gmail.com>
2022-01-03 16:23:10 +0200

    123213

--- swiper.js
+++ swiper.js
@@ -1,16 +1,12 @@
 import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js';
 
 new Swiper('.banners__slider', {
-   slidesPerView: 1,
+   slidesPerView: 2,
    touchRatio: 1,
    loop: true,
    slidesPerGroup: 1,
    grabCursor: true,
    breakpoints: {
-      576: {
-         slidesPerView: 2,
-
-      },
       991: {
          slidesPerView: 3,
 
